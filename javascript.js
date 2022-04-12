f0d9320189bfea407b338e16eaace303f27a2acf
Victor Martinez <victorlorenzomartinez@gmail.com>
2022-04-12 15:31:36 +0200

    created random rock/paper/scissors generating function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log('Hello World!')
+// Welcome user, ask for choice
+// Generate computer's choice with random number
+function computerPlay() {
+    let computerSelection = Math.floor(Math.random()*3);
+    if (computerSelection === 0) { 
+        return 'Rock';
+    } else if (computerSelection === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+console.log (computerPlay());
+// Apoint number values to choices
+// Decide who wins, use logic path
+// Make game end after 5 wins/losses
